4ec4d2de405d868df3444e2febdc27989c7b6d77
Hershal0-0 <hershalrao@gmail.com>
2024-06-27 02:14:05 +0530

    changes in navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     config.resolve.alias.canvas = false;
+//     config.resolve.alias.encoding = false;
+//     return config;
+//   },
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
@@ -7,6 +21,19 @@ const nextConfig = {
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "",
+          },
+        ],
+      },
+    ];
   },
 };
 
